86e3cc67a43caeecd01263a60e734e4fe174a8d7
Florian Thiel <flth@flth.de>
2023-09-08 01:23:12 +0200

    initial

---
+++ res/config/multiple_unit/br420plus.lua
@@ -0,0 +1,12 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/br420plus/br420_plus_1.mdl", forward = true },
+		{ name = "vehicle/waggon/br421plus/br421_plus.mdl", forward = true },
+        { name = "vehicle/train/br420plus/br420_plus_2.mdl", forward = false },
+	},
+	name = _("br420_plus_name"),
+	desc = _("br420_plus_desc"),
+    groupFileName = "vehicle/train/br420/menu_br420.mdl",
+}
+end
---
+++ strings.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "DB Class 420plus",
+		["mod_desc"] = "From two class 420 trains, the class 420plus was created in 2006 through conversion work. These were extensively rebuilt and modernised, including the installation of air-conditioning and windows so that it was possible to see from one carriage into the other. The interiors were also modernised and adapted to the class 423. The class 420plus was only in service on the S-Bahn Stuttgart network and was taken out of service in January 2014.\nRequires the mod of the class 420 from version 2.",
+
+        ["br420_plus_name"] = "BR 420 plus traffic red (2006)",
+		["br420_plus_desc"] = "Two BR 420 trains were converted to BR 420plus and an attempt was made to keep the vehicles in service longer through extensive modernisation. As they did not prove themselves, they were withdrawn together with other BR 420s in 2014.",
+
+	},
+	de = {
+
+		["mod_name"] = "BR 420plus",
+		["mod_desc"] = "Von zwei Zügen der Baureihe 420 wurde im Jahr 2006 durch Umbauarbeiten die Baureihe 420plus geschaffen. Diese wurden umfangreich umgebaut und modernisiert, u.a. wurden Klimaanlagen eingebaut und Fenster, um von einem Wagen in den anderen sehen zu können. Auch die Innenräume wurden modernisiert und an die Baureihe 423 angepasst. Die Baureihe 420plus war nur im Netz der S-Bahn Stuttgart unterwegs und wurde im Januar 2014 ausser Dienst gestellt.\nBenötigt den Mod der BR 420 ab Version 2.",
+
+		["br420_plus_name"] = "BR 420plus verkehrsrot (2006)",
+		["br420_plus_desc"] = "Zwei Züge der BR 420 wurden zur BR 420plus umgebaut und man versuchte durch umfangreiche Modernisierungen die Fahrzeuge länger im Einsatz zu halten. Da sie sich nicht bewährten, wurden sie zusammen mit anderen BR 420 im Jahr 2014 abgestellt.",
+
+	},
+}
+end
